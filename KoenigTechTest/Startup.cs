61d714727bf410258250111a92bbb0f8eeee1826
TMishin <mishintimur@gmail.com>
2016-01-23 16:31:01 +0200

    1st commit

---
+++ KoenigTechTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KoenigTechTest.Startup))]
+namespace KoenigTechTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
