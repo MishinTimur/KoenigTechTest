895915a14e833bf58281ff314a3c1ff3b2655127
TMishin <mishintimur@gmail.com>
2016-01-25 20:55:01 +0200

    FIX

---
+++ DataAccessLayer/EF/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace DataAccessLayer.EF
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
