71c91213897a129a26957477a46af58f9b3d4107
TMishin <mishintimur@gmail.com>
2016-01-28 14:57:02 +0200

    done

--- KoenigTechTest/App_Start/RouteConfig.cs
+++ KoenigTechTest/App_Start/RouteConfig.cs
@@ -12,16 +12,8 @@ namespace KoenigTechTest
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-//            routes.MapRoute(
-//                name: "account",
-//                url: "account",
-//                defaults: new {controller = "Home", action = "Index"});
-//            routes.MapRoute(
-//                name: "signup",
-//                url: "signup",
-//                defaults: new { controller = "Account", action = "Login" });
-//            routes.MapRoute("accountControllerRoute", "{action}", new {controller = "Account"}, new {action = "^Login$|^Register$|^LogOff$"});
-//            routes.MapRoute("Default", "account", new { controller = "Home", action="Index" });
+            routes.MapRoute("accountControllerRoute", "{action}", new {controller = "Account"}, new {action = "^Login$|^Register$|^LogOff$"});
+            routes.MapRoute("Default", "Account", new { controller = "Home", action="Index" });
             routes.MapRoute(
                 name: "common",
                 url: "{controller}/{action}",
